3b40a6793bbf30833f5ffc7ddf5a2c0f1fc50757
Emmanuel Cordoba <emmanuelc27@gmail.com>
2024-11-26 17:42:51 -0300

    Personalización de la _RecipesCard.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,7 +6,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: Column(children: <Widget>[
+        _RecipesCard(context),
+        _RecipesCard(context)],),
+    );
+  }
+
+  Widget _RecipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
         width: MediaQuery.of(context).size.width,
         height: 125,
         child: Card(
@@ -15,23 +24,27 @@ class HomeScreen extends StatelessWidget {
               Container(
                 height: 125,
                 width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
-                ),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(12)
+                )
               ),
               SizedBox(
                 width: 26,
               ),
               Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Milanesa Napolitana'),
-                  Text('Emmanuel Córdoba'),
+                  Text('Milanesa Napolitana', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
+                  SizedBox(height: 4,),
                   Container(
                     height: 2,
                     width: 150,
                     color: Colors.orange,
-                  )
+                  ),
+                  SizedBox(height: 4,),
+                  Text('Emmanuel Córdoba', style: TextStyle(fontSize: 14, fontFamily: 'Quicksand')),
                 ],
               )
             ],
