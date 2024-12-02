849c00e836bf0a91abc565ce524e09a08fc30aff
Emmanuel Cordoba <emmanuelc27@gmail.com>
2024-12-02 20:54:43 -0300

    Agrego paquete http. Agrego llamada a la API.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:platzi_recipe_book/screens/recipe_detail.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<List<dynamic>> FetchRecipes() async {
+    final url = Uri.parse('http://localhost:3001/recipes');
+    final response = await http.get(url);
+    final data = jsonDecode(response.body);
+    return data['recipes'];
+  }
+
   @override
   Widget build(BuildContext context) {
+    FetchRecipes();
     return Scaffold(
       body: Column(
         children: <Widget>[_RecipesCard(context), _RecipesCard(context)],
