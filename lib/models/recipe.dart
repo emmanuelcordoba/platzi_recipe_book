7b7f0ef8b11a337fb62729553c1f8c79f7d52087
Emmanuel Cordoba <emmanuelc27@gmail.com>
2024-12-03 15:57:18 -0300

    Agrego modelo Recipe. Gestión del Estado con Provider en Flutter.

---
+++ lib/models/recipe.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Recipe {
+  String name;
+  String author;
+  String imageLink;
+  List<String> steps;
+
+  Recipe({
+    required this.name,
+    required this.author,
+    required this.imageLink,
+    required this.steps,
+  });
+
+  factory Recipe.toJson( Map<String, dynamic> json){
+    return Recipe(
+      name: json['name'],
+      author: json['author'],
+      imageLink: json['image_link'],
+      steps: List<String>.from(json['recipe']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'author': author,
+      'image_link': imageLink,
+      'recipe': steps, 
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Recipe{name: $name, author: $author, imageLink: $imageLink, steps: $steps}';
+  }
+}
